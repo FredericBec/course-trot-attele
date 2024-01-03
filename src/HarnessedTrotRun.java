390f60d217b236fa79fc1980e0047a8351749bfa
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-03 15:54:13 +0100

    ajout méthode dice et horseSpeed

--- src/HarnessedTrotRun.java
+++ src/HarnessedTrotRun.java
@@ -1,9 +1,27 @@
+import java.util.Random;
 
 public class HarnessedTrotRun {
 
+	public static final int startLine = 0;
+	public static final int finishLine = 2400;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println(dice());
+		
+	}
+	
+	public static int dice() {
+		
+		int dice = new Random().nextInt(6);
+		
+		return dice;
+	}
+	
+	public static int horseSpeed(int dice) {
+		int speed = 0;
+		
+		return speed;
 	}
 
 }
