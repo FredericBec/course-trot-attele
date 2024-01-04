4ab1a1151af8040d18b59a5c8405145ae13a5a6e
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-04 11:02:22 +0100

    ajout méthode validInput et runChoice

--- src/HarnessedTrotRun.java
+++ src/HarnessedTrotRun.java
@@ -1,10 +1,12 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class HarnessedTrotRun {
 
 	public static final int startLine = 0;
 	public static final int finishLine = 2400;
+	public static final int actualSpeed = 0;
 	public static final int[] zeroSpeed = {0, 1, 1, 1, 2, 2};
 	public static final int[] firstSpeed = {0, 0, 1, 1, 1, 2};
 	public static final int[] secondSpeed = {0, 0, 1, 1, 1, 2};
@@ -13,13 +15,58 @@ public class HarnessedTrotRun {
 	public static final int[] fifthSpeed = {-2, -1, 0, 0, 0, 1};
 	public static final int[] sixthSpeed = {-2, -1, 0, 0, 0, 0};
 	
+	public static Scanner scan = new Scanner(System.in);
+	
 	
 	public static void main(String[] args) {
 		
-		int actualSpeed = 3;
-		System.out.println(distance(actualSpeed));
 		
 		
+		
+	}
+	
+	public static int validInput(Scanner scan, String errorMessage) {
+		int input = 0;
+		boolean validInput = false;
+
+		while (!validInput) {
+			if (scan.hasNextInt()) {
+				input = scan.nextInt();
+				validInput = true;
+			} else {
+				System.out.println(errorMessage);
+				scan.next();
+			}
+		}
+
+		return input;
+	}
+	
+	public static String runChoice() {
+		String choice = "";
+		HashMap<Integer, String> choiceList = new HashMap<Integer, String>();
+		choiceList.put(1, "tierce");
+		choiceList.put(2, "quarte");
+		choiceList.put(3, "quinte");
+		
+		for(int i : choiceList.keySet()) {
+			System.out.print("[" + i + " - " + choiceList.get(i) + "]");
+		}
+		System.out.println();
+		
+		System.out.println("Quel type de course souhaitez-vous?(entrez un numéro)");
+		boolean ok = false;
+		
+		while(!ok) {
+			int userChoice = validInput(scan, "Veuillez entrer un numéro valide !!");
+			if(choiceList.containsKey(userChoice)) {
+				choice = choiceList.get(userChoice);
+				ok = true;
+			}else System.out.println("Veuillez entrer un numéro de la liste !");
+				
+		}
+		
+		return choice;
 	}
 	
 	public static int dice() {
@@ -75,5 +122,7 @@ public class HarnessedTrotRun {
 		return actualDistance;
 		
 	}
+	
+	
 
 }
